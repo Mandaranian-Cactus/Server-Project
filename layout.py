6130dce89a413f24a457add45fcf88c81c95408d
Mandaranian-Cactus <47479122+Mandaranian-Cactus@users.noreply.github.com>
2019-10-21 18:24:12 -0400

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,7 +1,10 @@
 import pygame
+# from dice import Dice
 from card import Card
+import time
 import random
-from deck import Deck
+# from deck import Deck
+
 
 class Layout:
     def __init__(self, x, y, w, h):
@@ -18,12 +21,12 @@ class Layout:
 
         return False
 
+
 class Deck(Layout):
 
     def __init__(self, x, y, w, h):
         super().__init__(x, y, w, h)    # Does init from Layout class
         self.deck = []
-        print(self.deck)
 
     def add(self, card):
         self.deck.append(card)
@@ -34,9 +37,27 @@ class Deck(Layout):
     def flip(self):
         return self.deck.pop(0)
 
-# define the RGB value
-# for white colour
-white = (255, 255, 255)
+
+class Dice(Layout):
+
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
+        self.num = -1
+        self.rolls = []
+        self.rolling = 0  # False
+        self.rolls.append(pygame.transform.scale(pygame.image.load(r"C:\Users\dannie\PycharmProjects\untitled\Images\Dice\1.png"), (self.w, self.h)))
+        self.rolls.append(pygame.transform.scale(pygame.image.load(r"C:\Users\dannie\PycharmProjects\untitled\Images\Dice\2.png"), (self.w, self.h)))
+        self.rolls.append(pygame.transform.scale(pygame.image.load(r"C:\Users\dannie\PycharmProjects\untitled\Images\Dice\3.png"), (self.w, self.h)))
+        self.rolls.append(pygame.transform.scale(pygame.image.load(r"C:\Users\dannie\PycharmProjects\untitled\Images\Dice\4.png"), (self.w, self.h)))
+        self.rolls.append(pygame.transform.scale(pygame.image.load(r"C:\Users\dannie\PycharmProjects\untitled\Images\Dice\5.png"), (self.w, self.h)))
+        self.rolls.append(pygame.transform.scale(pygame.image.load(r"C:\Users\dannie\PycharmProjects\untitled\Images\Dice\6.png"), (self.w, self.h)))
+
+    def draw(self):
+        display_surface.blit(self.rolls[self.num], (self.x, self.y))
+
+    def roll(self):
+        self.num = random.randrange(0, 6)
+
 
 # assigning values to X and Y variable
 W = 900
@@ -48,20 +69,22 @@ display_surface = pygame.display.set_mode((W, H))
 image = pygame.image.load(r"C:\Users\dannie\PycharmProjects\untitled\Images\Layout.png")
 image = pygame.transform.scale(image, (900, 650))
 center = Layout(190, 185, 441, 253)
-private_hand = Layout(605, 480, 295, 170)
-gear_hands = []
-locations = ["" for i in range(40)]
-gear_hands.append(Layout(188, 517, 338, 101))
-gear_hands.append(Layout(707, 80, 117, 316))
-gear_hands.append(Layout(212, 18, 349, 121))
-gear_hands.append(Layout(13, 157, 89, 317))
+private_hand = Deck(605, 480, 295, 170)  # NOTE THAT THE DECK CLASS PROB ISN'T THE MOST FITTING FOR THIS (Change later)
+gear_regions = []  # Locations of gear hands
+gear_regions.append(Layout(188, 517, 338, 101))
+gear_regions.append(Layout(707, 80, 117, 316))
+gear_regions.append(Layout(212, 18, 349, 121))
+gear_regions.append(Layout(13, 157, 89, 317))
 
 # Add to Deck
-deck = Deck(208, 209, 49, 89)
-deck.add(Card(r"C:\Users\dannie\PycharmProjects\untitled\Images\Sun.png", 200, 200, 25, 40))
-deck.add(Card(r"C:\Users\dannie\PycharmProjects\untitled\Images\Spaghetti_Jesus.png", 300, 200, 25, 40))
-deck.add(Card(r"C:\Users\dannie\PycharmProjects\untitled\Images\FireHawk.png", 100, 200, 25, 40))
-deck.randomize()
+door_deck = Deck(208, 209, 49, 89)
+door_deck.add(Card(r"C:\Users\dannie\PycharmProjects\untitled\Images\Sun.png", 200, 200, 25, 40))
+door_deck.add(Card(r"C:\Users\dannie\PycharmProjects\untitled\Images\Spaghetti_Jesus.png", 300, 200, 25, 40))
+door_deck.add(Card(r"C:\Users\dannie\PycharmProjects\untitled\Images\FireHawk.png", 100, 200, 25, 40))
+door_deck.randomize()
+
+# Die
+die = Dice(280, 210, 50, 50)
 
 # infinite loop
 while True:
@@ -69,16 +92,28 @@ while True:
     # Draw background image
     display_surface.blit(image, (0, 0))
 
+    # Check to see if Die is being rolled
+    if 0 < die.rolling <= 50:   # Basically 50-micro rolls
+        die.roll()
+        die.rolling += 1
+    else:   # Reset die to be not rolling (0 = False)
+        die.rolling = 0
+
     for event in pygame.event.get():
-    # FIXXXXX
-        # For checking clicks on decks
+        # For checking clicks on decks (DOOR DECK ONLY RN)
         if event.type == pygame.MOUSEBUTTONUP:
             x, y = pygame.mouse.get_pos()
-            if deck.within_border(x, y, 1, 1):
-                card = deck.flip()
-                card.x, card.y = 650, 500   # Send to hand
+            if door_deck.within_border(x, y, 1, 1):
+                if len(door_deck.deck) > 0:
+                    card = door_deck.flip()
+                    card.x, card.y = 650, 500   # Send to private hand
+                    private_hand.add(card)  # Instantly adds the card into the private hand
 
-    # FIXXXXXX
+        # For checking click on Die
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            if die.within_border(x, y, 1, 1):
+                die.rolling = 1  # Initiate rolling (1 = True)
 
         # if event object type is QUIT
         # then quitting the pygame
@@ -86,18 +121,15 @@ while True:
         if event.type == pygame.QUIT:
             # quit the program.
             quit()
+            pygame.quit()
 
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #     pos = pygame.mouse.get_pos()
-        #     print(pos)
-
-        for i, card in enumerate(deck.deck):
+        for i, card in enumerate(private_hand.deck):
 
             # NOTE THAT BREAK STATEMENTS PREVENT OVERLAP
             # AKA, YOU CAN ONLY INTERACT WITH 1 OBJECT AT A TIME (Selection is based on it's order within list)
 
             # Actually do movement
-            if card.move:
+            if card.move:   # Bool to see if movement is toggled on
                 card.move_to(event)
                 break
 
@@ -108,40 +140,42 @@ while True:
                     card.move = True
                     break
 
-            # No break statement needed here
-            # See if to zoom up
-            if card.zoom_up(): card.zoom_bool = True    # Zoom up
-            else: card.zoom_bool = False    # Done zooming up
+    # Draw die
+    die.draw()
 
+    # Edits card locations
+    # Begin drawing deck
+    for i, card in enumerate(private_hand.deck):
 
-    for i, card in enumerate(deck.deck):
+        flag = False    # Flag used to record if card was at any gear regions at any time
 
         # Check/Edit positions of the cards
-        for gear_hand in gear_hands:
-            flag = False    # Flag to see if card is at a gear hand or not (Later flag gonna be removed)
-            if gear_hand.within_border(card.x, card.y, card.w, card.h):
-                locations[i] = "gear hand"
+        for gear_region in gear_regions:    # Check each gear hand region
+            if gear_region.within_border(card.x, card.y, card.w, card.h):
+                card.location = "gear hand"
                 flag = True
                 break
-            elif center.within_border(card.x, card.y, card.w, card.h):
-                locations[i] = "center"
-                flag = True
-                break
+
+        if not flag:
+            if center.within_border(card.x, card.y, card.w, card.h):
+                card.location = "center"
             elif private_hand.within_border(card.x, card.y, card.w, card.h):
-                locations[i] = "private hand"
-                flag = True
-                break
-
-        if not flag: locations[i] = ""
+                card.location = "private hand"
+            else:
+                card.location = ""
 
 
-        # Draw image
+        # Draw image (unzoomed and default)
         card.draw_unzoom()
+        card.zoom_up()
 
-    for i, card in enumerate(deck.deck):  # For overlap issues
-        if card.zoom_bool: card.draw_zoom()  # Draw zoomed up version
 
     # Redraw everything
     pygame.display.update()
-    # print(locations)
 
+    locations = []
+    # print(len(private_hand.deck))
+    for card in private_hand.deck:
+        locations.append(card.location)
+
+    print(locations)
